6ad657848453c1fa2e78bbd4419fd58957240ce1
sonnenfeld269 <david.sonnenfeld269@gmail.com>
2017-01-16 14:24:48 +0100

    enable scroll for all nav links

--- js/portfolio.js
+++ js/portfolio.js
@@ -2,19 +2,8 @@
   Here are some jquery functions that will be called to animate our index.html
 */
 $(document).ready(function() {
-
-    $(".arcUp").arctext({radius: 600});
-
-    $(".arcDown").arctext({radius: 600, dir: -1});
-
-    $('.trick').hover(function() {
-        $(this).addClass('animated bounce')
-    }, function() {
-        $(this).removeClass('animated bounce')
-    });
-
     // making a smooth scrolling
-    $(".navbar ul li a[href^='#']").on('click', function(e) {
+    $(".navbar a[href^='#']").on('click', function(e) {
         e.preventDefault();
 
         $('html, body').animate({
@@ -23,4 +12,7 @@ $(document).ready(function() {
             window.location.hash = this.hash;
         });
     });
+
+    // making a typed animation
+    $(".typed").typed({stringsElement: $('#typed-strings'), typeSpeed: 70});
 });
